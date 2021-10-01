393db5971e17ff964954859dbe96476c020a8ff7
Artowod <myjavaname@gmail.com>
2021-10-01 09:09:54 +0300

    task1 and 2 - done
    1 done
    video player - done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryArea = document.querySelector('.gallery');
+let galleryItem = '';
+galleryItems.forEach(({ preview, original, description }, index) => {
+  galleryItem += `
+<a class="gallery__item" href="${original}">
+<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+});
+
+galleryArea.insertAdjacentHTML('beforeend', galleryItem);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+/* lightbox.on('error.simplelightbox', e => {
+  console.log(`what is wrong: ${e}`);
+  alert('IMAGE LOAD ERROR !!! Please check images location!');
+}); */
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+/* 
+Ознакомься с документацией библиотеки Vimeo плеера.
+Добавь библиотеку как зависимость проекта через npm.
+Инициализируй плеер в файле скрипта как это описано в секции pre-existing player, 
+но учти что у тебя плеер добавлен как npm пакет, а не через CDN.
+Разбери документацию метода on() и начни отслеживать событие timeupdate - 
+обновление времени воспроизведения.
+Сохраняй время воспроизведения в локальное хранилище. Пусть ключом для хранилища 
+будет строка "videoplayer-current-time".
+При перезагрузке страницы воспользуйся методом setCurrentTime() для того чтобы 
+возобновить воспроизведение с сохраненной позиции.
+Добавь в проект бибилотеку lodash.throttle и сделай так, чтобы время 
+воспроизведения обновлялось в хранилище не чаще чем раз в секунду. */
 
+import Player from '@vimeo/player';
+import _ from 'lodash';
+
+const iframe = document.querySelector('iframe');
+const player = new Player('vimeo-player', iframe);
+
+const currentTime =
+  localStorage.getItem('videoplayer-current-time') === 'null'
+    ? 0
+    : localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime).then(seconds => {
+  console.log(seconds, 'sec');
+});
+
+player.on(
+  'timeupdate',
+  _.throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    });
+  }, 1000),
+);
